bee7c6e33370b520fac2427e4ede5013de7e3199
unknown <SENA@VALAPRCBCFSD107.FormacionSena.RED>
2021-10-21 09:46:09 -0500

    Cambios navbar

--- registrar_especialidad.php
+++ registrar_especialidad.php
@@ -7,10 +7,17 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="css/styles.css" rel="stylesheet" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <title>Registro de Especialidad</title>
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- Custom styles for this template -->
+  <link href="css/simple-sidebar.css" rel="stylesheet">
+  <title>Registrar Especialidad |</title>
 </head>
 <body>
-  
+<?php  
+   include('nabvar.php');
+
+?> 
   <div id="div1"  class="container "  >
    
     
