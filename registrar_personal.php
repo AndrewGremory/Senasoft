a628e61e0c816d7db078b83ee1ee54fe7eba936b
EdithDiflido <ediflido@misena.edu.co>
2021-10-20 22:45:50 -0500

    estilo personal

--- registrar_personal.php
+++ registrar_personal.php
@@ -9,55 +9,48 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="estilos.css"/>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <title>Document</title>
 </head>
 <body>
   
-<div class="w3-container "  style="width: 100%; padding: 50px 20px ;">
+  <div id="div1"  class="container "  style="width: 50%; padding: 50px 20px ;">
    
     
    <div class="w3-card-4">
-     <div class="w3-container w3-blue ">
+     <div class="container blue ">
        <h2>Registro del Personal</h2>
-     </div>
- 
- 
-                         
-      
-       <form  method="post"  action="guardar_personal.php" class="w3-container"> 
+     </div>                   
+        <form  method="post"  action="guardar_personal.php" class="w3-container"> 
    
-       <p>        
-        
-       <br>
-       
-          
-       <br>
+      
        <div class="w3-col ">
          <label class="label">Nombre y Apellido </label>
-         <input class="w3-input w3-border"  style="text-transform: uppercase;" type="text" name="nombre" id="nombre" value="" placeholder="Nombres" required/>
+         <input class="form-control"  style="text-transform: uppercase;" type="text" name="nombre" id="nombre" value="" placeholder="Nombres" required/>
        </div>
        <div class="w3-col m8 l6 ">
        
          <label class="label">Documento</label>
-         <input class="w3-input w3-border"  type="number" name="numdni" id="numdni" min="9999" max="9999999999999" value="" placeholder="Numero de identificación" required/> 
+         <input class="form-control"  type="number" name="numdni" id="numdni" min="9999" max="9999999999999" value="" placeholder="Numero de identificación" required/> 
        </div>
        <div class="w3-col ">
          <label class="label">Dirección</label>
-         <input class="w3-input w3-border"  style="text-transform: uppercase;" type="text" name="direccion" id="direccion" value="" placeholder="Dirección" required/>
+         <input class="form-control"  style="text-transform: uppercase;" type="text" name="direccion" id="direccion" value="" placeholder="Dirección" required/>
        </div>
        <div class="w3-col ">
          <label class="label">Teléfono</label>
-         <input class="w3-input w3-border"  style="text-transform: uppercase;" type="text" name="telefono" id="telefono"value="" placeholder="Teléfono" required/>
+         <input class="form-control"  style="text-transform: uppercase;" type="text" name="telefono" id="telefono"value="" placeholder="Teléfono" required/>
        </div>
        <div class="w3-col ">
          <label class="label">Email</label>
-         <input class="w3-input w3-border"  style="text-transform: uppercase;" type="email" name="email" id="email" value="" placeholder="Correo electronico" required/>
+         <input class="form-control"  style="text-transform: uppercase;" type="email" name="email" id="email" value="" placeholder="Correo electronico" required/>
        </div>
        
        <div class="w3-col ">
           <label class="label">Especialidad</label>
             
-          <select class="w3-select" name="rol" id="rol">
+          <select class="form-select" name="especialidad" id="especialidad">
             <?php
               if ($resultado>0)
               {
@@ -76,13 +69,9 @@
 <br>
 <br>
 <br>
-       <div class="w3-row-padding">
- <div class="w3-half"> 
- <a  href="propietarios.php" class="w3-button w3-blue" style="width:100% "> Cancelar</a>
- </div>
- <div class="w3-half">
- <button class="w3-bar-item w3-button w3-blue" style="width:100%">Registrar</button>
- </div>
+<div class="d-grid gap-2 d-md-block">
+<a class="btn btn-primary btn-lg" href="guardar_personal.php" role="button">Registrar</a>
+<a class="btn btn-primary btn-lg" href="#" role="button">Cancelar</a>
 </div>
 
        <br>
