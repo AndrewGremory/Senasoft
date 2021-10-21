b4b78fe6af91a947cea21177028223894779d0c6
unknown <SENA@VALAPRCBCFSD107.FormacionSena.RED>
2021-10-21 16:07:28 -0500

    Cambios inicio k

--- inicio.php
+++ inicio.php
@@ -12,21 +12,33 @@
     <title>Inicio |</title>
 </head>
 <body>
-
-<?php  
-   include('nabvar.php');
-
-?> 
+    
+<nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+               nombre del usuario
+              </a>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="cerrar_sesion.php">Cerrar Sesión</a>
+                <a class="dropdown-item" href="registrar_personal.php">Registro Personal</a>
+                <a class="dropdown-item" href="registrar_titurnos.php">Registro Tipo Turno</a>
+                <a class="dropdown-item" href="registrar_rol.php">Registro Usuario</a>
+                <a class="dropdown-item" href="Registrar_especialidad.php">Registro Especialidad</a>
+                <a class="dropdown-item" href="Registrar_rol.php">Registro Rol</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
 
     <div style="padding-left:16px">
-    <button class="button" style="vertical-align:middle"><span>Agregar Servicio </span></button>
-    <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0" style="width:38%">
-                <div class="input-group">
-                    <input class="form-control" type="text" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2" />
-
-                </div>
-            </form>
-    <hr>
+    <button class="button" style="vertical-align:middle"><span>Agregar Servicio </span></button><hr>
     <div class="contenido">
         <center><div class="contenido-b">
         <button class="button" style="vertical-align:middle"><span>Radioterapia </span></button>
@@ -48,6 +60,8 @@
 
         </div></center>
     </div>
-
+      <!-- Bootstrap core JavaScript -->
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
