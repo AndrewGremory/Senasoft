a064c053bbe801ee24d547efd94f5c7cfea2b8d0
unknown <SENA@VALAPRCBCFSD107.FormacionSena.RED>
2021-10-21 16:24:31 -0500

    Cambios inicio 2k

--- inicio.php
+++ inicio.php
@@ -12,31 +12,11 @@
     <title>Inicio |</title>
 </head>
 <body>
-    
-<nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-               nombre del usuario
-              </a>
-              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="cerrar_sesion.php">Cerrar Sesión</a>
-                <a class="dropdown-item" href="registrar_personal.php">Registro Personal</a>
-                <a class="dropdown-item" href="registrar_titurnos.php">Registro Tipo Turno</a>
-                <a class="dropdown-item" href="registrar_rol.php">Registro Usuario</a>
-                <a class="dropdown-item" href="Registrar_especialidad.php">Registro Especialidad</a>
-                <a class="dropdown-item" href="Registrar_rol.php">Registro Rol</a>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </nav>
+   
+<?php  
+   include('nabvar.php');
 
+?> 
     <div style="padding-left:16px">
     <button class="button" style="vertical-align:middle"><span>Agregar Servicio </span></button><hr>
     <div class="contenido">
@@ -54,14 +34,7 @@
         <button class="button" style="vertical-align:middle"><span>Agregar Servicio </span></button>
         <button class="button" style="vertical-align:middle"><span>Agregar Servicio </span></button>
 
-
-
-                
-
         </div></center>
     </div>
-      <!-- Bootstrap core JavaScript -->
-  <script src="js/jquery.min.js"></script>
-  <script src="js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
